0f30f6efeb79f711143012637a58bc28c4e0eb37
eric <howcrazys@gmail.com>
2024-03-18 11:41:40 +0800

    feat(custom): pre commit lint, run test

---
+++ .cz-config.cjs
@@ -0,0 +1,27 @@
+// 
+module.exports = 'use strict';
+
+module.exports = {
+  types: [
+    {
+      value: 'feat',
+      name : 'feat: 新增功能。'
+    },
+    {
+      value: 'fix',
+      name : 'fix: 修復 bug。'
+    }
+  ],
+
+  scopes: [
+ 
+  ],
+
+  messages: {
+
+  },
+  allowBreakingChanges: ["feat", "fix", "refactor"],
+};;
+
+
+
